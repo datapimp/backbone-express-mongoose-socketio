f06aa7838760b94e9529604f73bb93d87e2568e5
Jonathan Soeder <jonathan.soeder@gmail.com>
2010-10-31 00:27:11 -0500

    Added yui compressor to build

--- public/js/application.sprockets.js
+++ public/js/application.sprockets.js
@@ -5,16 +5,23 @@ var Application = {
   channels : {}
 };
 
+// these files get pulled in from sprockets
+
+// where I configure the backbone models / collections
 //= require "application/models"
 //= require "application/collections"
+
+// where I define general behavior of the socket
 //= require "application/socket"
+
+// where I define specific behavior for various channels on the socket
 //= require "application/channels"
 
 Application.run = function(){
-  console.log('Running Application...');
   Application.socket.connect();
 };
 
+// when the remmy is in the system
 $(function(){
   Application.run();
 });
--- public/js/application/socket.js
+++ public/js/application/socket.js
@@ -1,11 +1,8 @@
 Application.socket = {
   provider : new io.Socket(),
   onConnect : function(){
-    console.log('Connected to Socket');
   },
   onMessage : function(p){
-    console.log('message', p );
-    
     var message = JSON.parse(p),
         channel = message.channel.split(/\:/).pop(),
         data = message.message;
@@ -18,18 +15,19 @@ Application.socket = {
       }
     }
     
-    console.log('Socket Message', channel, data, typeof(data) );
-    
     if( Application.channels[channel] && typeof(Application.channels[channel]=="function") ){
       Application.channels[channel](data);
     }
   },
   onDisconnect : function(){
-    console.log('Socket Disconnected');
+    setInterval(Application.socket.connect,300);
   },
   connect : function(){
-    console.log('Attempting to connect to socket...');
     var s = Application.socket.provider;
+    
+    if(f.connected){
+      return;
+    }
     
     s.on("connect", Application.socket.onConnect );
     s.on("message", Application.socket.onMessage );
